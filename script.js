619af7a0a6ad3d592856b66ca32e2608af5e06fc
WillianAmorim <willian.amorim1@outlook.com>
2021-06-10 09:50:16 -0300

    desafio 7 concluido

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ let button = document.querySelector('#criar-tarefa')
 let lista = document.getElementById('lista-tarefas');
 let input = document.getElementById('texto-tarefa');
 
-function criarLista(){
+function adicionarTarefa(){
 let li = document.createElement('li')
 
 lista.appendChild(li)
 li.innerText = input.value
 input.value = ''
+li.addEventListener('click', alterarCorDeFundo)
 }
 
-button.addEventListener('click', criarLista)
+button.addEventListener('click', adicionarTarefa)
+
+
+function alterarCorDeFundo(event){
+    let tarefa = event.target
+    tarefa.style.backgroundColor = 'rgb(128,128,128)'
+}
+
+
+
+
+
+
 
 

08ad7dff48bf28ed4bbcaf63f7ee08305387e9ad
WillianAmorim <willian.amorim1@outlook.com>
2021-06-15 19:42:11 -0300

    feito requisito 8

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ let button = document.querySelector('#criar-tarefa')
 let lista = document.getElementById('lista-tarefas');
 let input = document.getElementById('texto-tarefa');
 
+
+let selecionaCor = document.createElement('class')
+
+function alterarCorDeFundo(event){
+    let tarefa = event.target
+    let classeSelected = document.querySelector('.selected')
+    if(classeSelected != null){
+        classeSelected.classList.remove('selected')
+    }
+    tarefa.classList.add('selected')
+    
+    
+}
+
 function adicionarTarefa(){
 let li = document.createElement('li')
 
@@ -13,15 +27,12 @@ li.addEventListener('click', alterarCorDeFundo)
 
 button.addEventListener('click', adicionarTarefa)
 
-
-function alterarCorDeFundo(event){
-    let tarefa = event.target
-    tarefa.style.backgroundColor = 'rgb(128,128,128)'
+/* function mudarCor () {
+    if (li = 'click'){
+        li = document.getElementsByTagName('li').classList.add('selected')
+    }
 }
-
-
-
-
+ */
 
 
 
